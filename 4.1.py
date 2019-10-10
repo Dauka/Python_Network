8c6935eb0a5d65638b451e822f853f19d0659ae1
DaurDi <d.ensebekov@gmail.com>
2019-10-10 09:13:37 +0600

    Add files via upload

---
+++ 4.1.py
@@ -0,0 +1,7 @@
+'''
+Обработать строку nat таким образом, чтобы в имени интерфейса вместо FastEthernet было GigabitEthernet.
+'''
+
+
+NAT = "ip nat inside source list ACL interface FastEthernet0/1 overload"
+print(NAT.replace('Fast','Gigabit'))
