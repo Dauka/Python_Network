fe423cfe6ede49add6e8ac52f0de5064c87c3776
DaurDi <d.ensebekov@gmail.com>
2019-10-16 13:27:13 +0600

    Delete 4.1.py

--- 4.1.py
+++
@@ -1,7 +0,0 @@
-'''
-Обработать строку nat таким образом, чтобы в имени интерфейса вместо FastEthernet было GigabitEthernet.
-'''
-
-
-NAT = "ip nat inside source list ACL interface FastEthernet0/1 overload"
-print(NAT.replace('Fast','Gigabit'))
